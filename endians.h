c4e3e06512a73ca23e27cce957d16e691c2087fe
Tom <elunico@users.noreply.github.com>
2022-05-05 18:05:56 -0400

    Added constexpr

--- endians.h
+++ endians.h
@@ -6,7 +6,7 @@
 #include <iostream>
 
 template <typename Integral, std::size_t count = sizeof(Integral)>
-std::array<uint8_t, count> get_le_bytes(Integral value) {
+constexpr std::array<uint8_t, count> get_le_bytes(Integral value) {
   std::array<uint8_t, count> result{};
   for (auto i = 0; i < sizeof(value); ++i) {
     result[i] = static_cast<uint8_t>(value & 0xFF);
@@ -16,17 +16,19 @@ std::array<uint8_t, count> get_le_bytes(Integral value) {
 }
 
 template <typename Integral, std::size_t count = sizeof(Integral)>
-std::array<uint8_t, count> get_be_bytes(Integral value) {
+constexpr std::array<uint8_t, count> get_be_bytes(Integral value) {
   auto result = get_le_bytes(value);
   std::reverse(result.begin(), result.end());
   return result;
 }
 
-template <> std::array<uint8_t, 1> get_le_bytes<uint8_t, 1>(uint8_t value) {
+template <>
+constexpr std::array<uint8_t, 1> get_le_bytes<uint8_t, 1>(uint8_t value) {
   return {value};
 }
 
-template <> std::array<uint8_t, 1> get_be_bytes<uint8_t, 1>(uint8_t value) {
+template <>
+constexpr std::array<uint8_t, 1> get_be_bytes<uint8_t, 1>(uint8_t value) {
   return {value};
 }
 
